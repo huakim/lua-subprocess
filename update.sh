eed5fde8d3410ca604c6d802f4c3d21b5d6be966
Your Name <you@example.com>
2024-07-06 11:19:42 +0300

    update

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rm *.o *.so *.tar.gz *.spec *.rockspec
-LUA_SUBPROCESS_VERSION=(git describe --tags | sed -r 's/([^-]*-[^-]*).*/\1/') python3 generate_spec.py
+LUA_SUBPROCESS_VERSION="$(git describe --tags | sed -r 's/([^-]*-[^-]*).*/\1/')" python3 generate_spec.py
 rm *.obsinfo
 git add --all
 git commit -m update
